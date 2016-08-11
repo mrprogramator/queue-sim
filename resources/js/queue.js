a5b9ef4c37a5329f1ba10414f089ecfddd9ec466
Luis Saucedo <luisaucedoquiroga@gmail.com>
2016-08-11 12:24:27 -0400

    Adding Service Time Simulation

--- resources/js/queue.js
+++ resources/js/queue.js
@@ -11,17 +11,38 @@ app.controller('QueueController', function ($scope, $timeout) {
     
     $scope.doneLength = 0;
     
-    $scope.queueFreq = 0.01;
+    $scope.queueFreq = 0.5;
     
     $scope.queue = [];
     
+    $scope.stDesv = 3.790599752559;
+    
+    $scope.ticketCount = 1;
+    
+    $scope.isSimulating = false;
+    
     $scope.simular = function () {
-        $scope.date = new Date();
+        if ($scope.isSimulating) return;
+        
+        $scope.isSimulating = true;
+        
+        $scope.queue = [];
+        $scope.ticketCount = 1;
+        $scope.doneLength = 0;
+        
+        $scope.date = new Date(0,0,0,0,0,0,0);
         loadServers();
         
         $scope.queue = [];
         
+        $scope.ticketCount = 1;
+        
         calculateQueue();
+    }
+    
+    $scope.stop = function () {
+        $scope.isSimulating = false;
+        $timeout.cancel($scope.mainTO);
     }
     
     function loadServers () {
@@ -36,6 +57,7 @@ app.controller('QueueController', function ($scope, $timeout) {
         for (var i=1; i <= $scope.numOfServers; ++i) {
             $scope.servers.push({
                 nroMesa: "M" + pad(i, size),
+                served: 0
             })
         }
     }
@@ -52,22 +74,75 @@ app.controller('QueueController', function ($scope, $timeout) {
     
     function generateQueue(q){
         var rand = Math.random();
-        console.log('generating queue....',rand, q);
         
         if(rand > q){
-            $scope.queue.push({x:'X'});
+            $scope.queue.push('T' + pad($scope.ticketCount));
+            $scope.ticketCount++;
         }
         
-        $timeout(function () {
-            document.getElementById('clock').innerHTML = $scope.date.toLocaleTimeString();
+        $scope.mainTO = $timeout(function () {
+            document.getElementById('clock').innerHTML = 
+            $scope.date.getUTCDay() + " día(s), "
+            + $scope.date.getHours() + " hora(s), " 
+            + $scope.date.getMinutes() + " minuto(s), "
+            + $scope.date.getSeconds()+ " segundo(s)";
             generateQueue(q);
+
             $scope.date.setMinutes($scope.date.getMinutes() + $scope.arriveTimes);
-        }, $scope.queueFreq*60*1000)
+            
+            $timeout(function (){
+                startPullingQueue();
+            }, 1000);
+            
+        }, $scope.queueFreq*1000)
+    }
+    
+    function call(server){
+        if (!$scope.isSimulating) return;
+        
+        if (server.nroTicket && server.nroTicket.length > 0) return;
+
+        var ticket = $scope.queue.shift(1);
+        if (ticket){
+            server.nroTicket = ticket;
+            startAttention(server);
+        }
+    }
+    
+    function startPullingQueue(){
+        $scope.servers.forEach(function (s) {
+            call(s);
+        })
+    }
+    
+    function startAttention(server){
+        call(server);
+        
+        var attTime = getX(Math.random(), $scope.attentionTime);
+        console.log(attTime);
+        $timeout(function () {
+            finishAttention(server);
+        },attTime * $scope.queueFreq * 1000);
+    }
+    
+    function finishAttention(server){
+        if (!$scope.isSimulating) return;
+        
+        server.served++;
+        $scope.doneLength++;
+        server.nroTicket = undefined;
+        $timeout(function (){
+            call(server);
+        }, $scope.queueFreq * 5000)
     }
     
     /*Helpers*/
     function getPoisson(lambda, time, n){
         return ( Math.pow(lambda*time,n) * Math.pow(Math.E,-lambda*time) )/ factorial(n);
+    }
+    
+    function getX(z, mean){
+        return (z*$scope.stDesv + mean)
     }
     
     function factorial(n) {
